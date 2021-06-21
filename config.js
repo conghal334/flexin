0a1dccb1a04328514fa9fe8de72740bda5f2e676
To Cong <72975273+conghal334@users.noreply.github.com>
2021-06-21 15:29:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-exports.Prefix = `L`;//your prefix for bot
-exports.Token = `ODQ4OTczNzMyNzM0MTczMTg0.YLUaZw.v22ZjYX_PPpHKkRlXTE4WNS4aP4`;//your token 
-exports.Color = `GREEN`;//color   of embed
-exports.Ytapi = `AIzaSyAM1JpfbD4o_C4HOhCfSHQMRrA3sMvjorE`;
+exports.Prefix = `L`;//prefix của bot
+exports.Token = `BOT TOKEN`;//bto token
+exports.Color = `GREEN`;//màu của embed
+exports.Ytapi = `YOUTUBE API`;
 exports.Maxplaylistsize = 10;
 exports.Staytime = 60;
 exports.Volume = 100;
 exports.Pruning = false;
 exports.Locale = `en`;
-exports.Mongo = `mongodb://conghal:Thuongvu1@flexinbot-shard-00-00.cesni.mongodb.net:27017,flexinbot-shard-00-01.cesni.mongodb.net:27017,flexinbot-shard-00-02.cesni.mongodb.net:27017/Flexinbot?replicaSet=atlas-fat2xt-shard-0&ssl=true&authSource=admin`;
+exports.Mongo = `Mongo_db URL`;
 exports.admins = ["677918899471122472"];
 exports.countChannel = "835554319004336238";
